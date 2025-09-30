0b019e326164f16fc5d7869c574c7a4c7126c678
Esben Nyboe Jacobsen <enj@hotmail.com>
2025-09-30 15:49:42 +0200

    For-loop

--- barbarianblaster/Turret/turret.gd
+++ barbarianblaster/Turret/turret.gd
@@ -6,7 +6,9 @@ extends Node3D
 var enemy_path: Path3D
 
 func _physics_process(delta: float) -> void:
-	var enemy = enemy_path.get_children().back()
+	var enemy = find_best_target()
+	if !enemy:
+		return
 	var corrected_enemy_position = enemy.global_position
 	corrected_enemy_position.y = global_position.y
 	look_at(corrected_enemy_position, Vector3.UP, true)
@@ -19,3 +21,13 @@ func shoot() -> void:
 	add_child(shot)	
 	shot.global_position = turret_barrel_2.global_position
 	shot.direction = global_transform.basis.z
+
+func find_best_target() -> PathFollow3D:
+	var best_target = null
+	var best_progress = 0
+	for enemy in enemy_path.get_children():
+		if enemy is PathFollow3D:
+			if enemy.progress > best_progress:
+				best_target = enemy
+				best_progress = enemy.progress
+	return best_target
