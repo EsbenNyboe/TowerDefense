eb64c58ab39642f20f235461db46632d9ef959c9
Esben Nyboe Jacobsen <enj@hotmail.com>
2025-09-30 13:36:11 +0200

    Setup Timers for projectiles

--- barbarianblaster/Turret/turret.gd
+++ barbarianblaster/Turret/turret.gd
@@ -1,7 +1,12 @@
 extends Node3D
 
 @export var projectile : PackedScene
+@onready var turret_barrel_2: MeshInstance3D = $TurretBase/TurretTop/TurretBarrel/TurretBarrel2
+
+func _on_timer_timeout() -> void:
+	shoot()
 
 func shoot() -> void:
 	var shot = projectile.instantiate()
 	add_child(shot)	
+	shot.global_position = turret_barrel_2.global_position
