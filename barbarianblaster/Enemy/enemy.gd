eabd657bfa726c8bbe58b2f1a695f63205131821
Esben Nyboe Jacobsen <enj@hotmail.com>
2025-09-30 14:09:17 +0200

    Destroy enemy and projectile on hit

--- barbarianblaster/Enemy/enemy.gd
+++ barbarianblaster/Enemy/enemy.gd
@@ -1,11 +1,24 @@
 extends PathFollow3D
 
 @export var speed: float = 2.5
+@export var max_health: int = 50
 
 @onready var base = get_tree().get_first_node_in_group("base")
+
+var health: int:
+	set(value):
+		health = value
+		if health < 1:
+			queue_free()
+
+func _ready() -> void:
+	health = max_health
 
 func _process(delta: float) -> void:
 	progress += delta * speed
 	if progress_ratio >= 1:
 		base.take_damage()
 		set_process(false)
+
+func take_damage(damage: int) -> void:
+	health -= damage
