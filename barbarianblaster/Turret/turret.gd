cb7e42dfc8f8686a4f46eb76a017f7d83d3551a7
Esben Nyboe Jacobsen <enj@hotmail.com>
2025-09-30 13:48:59 +0200

    Look at enemy

--- barbarianblaster/Turret/turret.gd
+++ barbarianblaster/Turret/turret.gd
@@ -3,6 +3,8 @@ extends Node3D
 @export var projectile : PackedScene
 @onready var turret_barrel_2: MeshInstance3D = $TurretBase/TurretTop/TurretBarrel/TurretBarrel2
 
+var enemy_path: Path3D
+
 func _on_timer_timeout() -> void:
 	shoot()
 
@@ -10,3 +12,9 @@ func shoot() -> void:
 	var shot = projectile.instantiate()
 	add_child(shot)	
 	shot.global_position = turret_barrel_2.global_position
+
+func _physics_process(delta: float) -> void:
+	var enemy = enemy_path.get_children().back()
+	var corrected_enemy_position = enemy.global_position
+	corrected_enemy_position.y = global_position.y
+	look_at(corrected_enemy_position, Vector3.UP, true)
