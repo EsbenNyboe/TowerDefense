f543255737cda1f4347f34bd9d2e1430ca2bda72
Esben Nyboe Jacobsen <enj@hotmail.com>
2025-10-01 09:15:27 +0200

    Signal direct map to method

--- barbarianblaster/Enemy/enemy_path.gd
+++ barbarianblaster/Enemy/enemy_path.gd
@@ -2,9 +2,6 @@ extends Path3D
 
 @export var enemy_scene: PackedScene
 
-func _on_timer_timeout() -> void:
-	spawn_enemy()
-
 func spawn_enemy() -> void:
 	var spawned_enemy = enemy_scene.instantiate()
 	add_child(spawned_enemy)
