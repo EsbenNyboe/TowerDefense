0483b56f9b20d4057d14e934122bfbc1fb9e328f
Esben Nyboe Jacobsen <enj@kanda.dk>
2025-10-03 13:32:29 +0200

    Modify jump-logic, and fix gravity-logic

--- robo-rampage/Player/player.gd
+++ robo-rampage/Player/player.gd
@@ -2,9 +2,11 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+
+@export var jump_height: float = 1.0
 
 var mouse_motion := Vector2.ZERO
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var camera_pivot: Node3D = $CameraPivot
 
@@ -31,13 +33,13 @@ func handle_camera_rotation() -> void:
 	mouse_motion = Vector2.ZERO
 
 func handle_movement(delta: float) -> void:
-	# Add the gravity.
+	# Add the gravity.	
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y -= gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = sqrt(jump_height * 2.0 * gravity)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
