e22f90055189bc33e43125c1fc0e5fe5a5ff754f
Esben Nyboe Jacobsen <enj@kanda.dk>
2025-10-03 13:02:49 +0200

    Smooth camera: Interpolate transform rotation

---
+++ robo-rampage/Player/smooth_camera.gd
@@ -0,0 +1,10 @@
+extends Camera3D
+
+@export var speed := 44.0
+
+func _physics_process(delta: float) -> void:
+	var weight = clamp(delta * speed, 0, 1)
+	global_transform = global_transform.interpolate_with(
+		get_parent().global_transform, weight
+	)
+	global_position = get_parent().global_position
