9417d1cf6ac6e70b6f286d334f83f50f94c3b3e1
Esben Nyboe Jacobsen <enj@kanda.dk>
2025-10-03 09:42:57 +0200

    Control camera with mouse motion (alternative input-event)

--- robo-rampage/Player/player.gd
+++ robo-rampage/Player/player.gd
@@ -4,8 +4,36 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+var mouse_motion := Vector2.ZERO
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
+	handle_camera_rotation()
+	
+	#if Input.is_action_just_pressed("ui_cancel"):
+		#if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			#Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		#else:
+			#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	handle_movement(delta)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		mouse_motion = -event.relative * 0.001
+	if event.is_action_pressed("ui_cancel"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func handle_camera_rotation() -> void:
+	rotate_y(mouse_motion.x)
+	mouse_motion = Vector2.ZERO
+
+func handle_movement(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
