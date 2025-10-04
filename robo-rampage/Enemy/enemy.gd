e1d57723a6366d1f30c1a898279aed1dc135bb8b
Esben <esbennj@hotmail.com>
2025-10-04 08:51:01 +0200

    Enemy attack  animation

--- robo-rampage/Enemy/enemy.gd
+++ robo-rampage/Enemy/enemy.gd
@@ -1,9 +1,12 @@
 extends CharacterBody3D
 
-
 const SPEED = 3.0
 
+@export var attack_range := 1.5
+
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 var player
 var provoked := false
 var aggro_range := 12.0
@@ -28,7 +31,11 @@ func _physics_process(delta: float) -> void:
 	if distance < aggro_range:
 		provoked = true
 	
+	if distance < attack_range:
+		animation_player.play("Attack")
+	
 	if direction:
+		look_at_target(direction)
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
@@ -36,3 +43,11 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func look_at_target(direction: Vector3) -> void:
+	var adjusted_direction = direction
+	adjusted_direction.y = 0
+	look_at(global_position + adjusted_direction, Vector3.UP, true)
+
+func attack() -> void:
+	print("Enemy Attack!")
