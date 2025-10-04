2ad36f0660b72355a7ebf5f39137b86d3b42b1b2
Esben <esbennj@hotmail.com>
2025-10-04 12:41:09 +0200

    Recoil (onready var, and lerp)

--- robo-rampage/Weapons/hitscan_weapon.gd
+++ robo-rampage/Weapons/hitscan_weapon.gd
@@ -1,17 +1,21 @@
 extends Node3D
 
 @export var fire_rate := 14.0
+@export var recoil := 0.05
+@export var weapon_mesh: Node3D
 
 @onready var cooldown_timer: Timer = $CooldownTimer
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+@onready var weapon_position: Vector3 = weapon_mesh.position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if Input.is_action_pressed("fire"):
 		if cooldown_timer.is_stopped():
-			cooldown_timer.start(1.0 / fire_rate)
-			print("fire")
+			shoot()
+	
+	weapon_mesh.position = weapon_mesh.position.lerp(weapon_position, delta * 10.0)
+
+func shoot() -> void:
+	cooldown_timer.start(1.0 / fire_rate)
+	print("fire")
+	weapon_mesh.position.z += recoil
