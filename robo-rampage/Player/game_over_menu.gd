ea1c3e3a9ba85f7d5e7f3c8f5e6c6d5d1a85b813
Esben <esbennj@hotmail.com>
2025-10-05 17:54:53 +0200

    Import font, setup theme, setup game over screen

---
+++ robo-rampage/Player/game_over_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+func _on_restart_button_pressed() -> void:
+	get_tree().reload_current_scene()
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+func game_over() -> void:
+	visible = true
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
