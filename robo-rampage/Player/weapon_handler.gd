0127cd9853f0868afd6fe517019a83509a840887
Esben <esbennj@hotmail.com>
2025-10-05 18:45:15 +0200

    Scroll to select weapon (make Control ignore mouse-input, so its not getting blocked)

--- robo-rampage/Player/weapon_handler.gd
+++ robo-rampage/Player/weapon_handler.gd
@@ -6,6 +6,19 @@ extends Node3D
 func _ready() -> void:
 	equip(weapon_2)
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("weapon_1"):
+		equip(weapon_1)
+	
+	if event.is_action_pressed("weapon_2"):
+		equip(weapon_2)
+	
+	if event.is_action_pressed("next_weapon"):
+		next_weapon()
+	
+	if event.is_action_pressed("previous_weapon"):
+		previous_weapon()
+
 func equip(active_weapon: Node3D) -> void:
 	for child in get_children():
 		if child == active_weapon:
@@ -15,9 +28,18 @@ func equip(active_weapon: Node3D) -> void:
 			child.visible = false
 			child.set_process(false)
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("weapon_1"):
-		equip(weapon_1)
-	
-	if event.is_action_pressed("weapon_2"):
-		equip(weapon_2)
+func next_weapon() -> void:
+	var index = get_current_index()
+	index = wrapi(index + 1, 0, get_child_count())
+	equip(get_child(index))
+
+func previous_weapon() -> void:
+	var index = get_current_index()
+	index = wrapi(index - 1, 0, get_child_count())
+	equip(get_child(index))
+
+func get_current_index() -> int:
+	for index in get_child_count():
+		if get_child(index).visible:
+			return index
+	return 0
